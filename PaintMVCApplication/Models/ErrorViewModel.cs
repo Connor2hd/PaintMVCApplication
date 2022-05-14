8b991d609d080481204ea68c63392177751c587d
Connor Mackay <Connor@goliathhosting.com>
2022-05-14 14:00:48 -0400

    Add project files.

---
+++ PaintMVCApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PaintMVCApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
